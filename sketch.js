a6295a9d9e124779c0bd0fc9505046060fe6709c
VyshanthS <115634282+VyshanthS@users.noreply.github.com>
2022-10-12 07:01:00 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(400,400);
+  var box = createSprite (200,200,20,20)
+  box.shapeColor = "red"
+  
+}
+
+function draw() 
+{
+  background(30);
+  drawSprites()
+
+}
+
+
+
+
